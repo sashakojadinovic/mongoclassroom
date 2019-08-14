943786c5612bceece6ef53a2ef872a304a320aa1
sashakojadinovic <stanojeglavas@msn.com>
2019-08-15 00:13:53 +0200

    Messadge dodlj commit

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const error = true
-function f(){
-    return new Promise((resolve,reject)=>{
-        if(!error){
-            resolve(fetch('https://jsonplaceholder.typicode.com/posts/3')
-            .then((data)=>data.json()
-            .then((data)=>data)))
-        }
-        else{
-            reject('ERR OCCURED')
-        }
-    })
-}
-f()
-    .then(data=>console.log(data.title))
-    .catch(e=>console.log(e))
+console.log("OK")
